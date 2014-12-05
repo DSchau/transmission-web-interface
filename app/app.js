fac8281774c52baf7b77d887516de1c04a9a69f3
Dustin Schau <dustinschau@gmail.com>
2014-12-05 03:10:15 -0600

    fix: Now uses deferred-angular-bootstrap to get transmision API key
    This is much cleaner.. safer, etc.

--- app/app.js
+++ app/app.js
@@ -1,10 +1,28 @@
+window.deferredBootstrapper.bootstrap({
+  element: document.body,
+  module: 'transmission',
+  resolve: {
+    API_KEY: ['$http', '$q', function ($http, $q) {
+      var defer = $q.defer();
+      $http.post('/transmission/rpc').error(function(data, status, headers) {
+        var headerObj = headers(),
+          token = 'X-Transmission-Session-Id',
+          key = headerObj[token.toLowerCase()];
+        defer.resolve(key);
+      });
+      return defer.promise;
+    }]
+  }
+});
+
 angular.module('transmission', [
   'ngRoute',
   'headroom',
   'transmission.torrents',
   'transmission.common'
 ])
-.config(function($routeProvider, $httpProvider) {
+.config(function($routeProvider, $httpProvider, API_KEY) {
+  $httpProvider.defaults.headers.common['X-Transmission-Session-Id'] = API_KEY;
   $httpProvider.interceptors.push('httpInterceptor');
   $routeProvider.otherwise({
     redirectTo:'/torrents'
--- app/torrents/_torrents.js
+++ app/torrents/_torrents.js
@@ -8,15 +8,8 @@ angular.module('transmission.torrents', [
     templateUrl: 'torrents/torrents.tpl.html',
     controller: 'TorrentsCtrl as torrents',
     resolve: {
-      torrents: function($q, $http, transmission, transmissionRPC, GET_TORRENTS) {
-        var defer = $q.defer();
-        transmissionRPC.init(null, function(data) {
-          $http.defaults.headers.common[transmission.token] = transmission[transmission.token];
-          transmissionRPC.torrents(GET_TORRENTS, function(response) {
-            defer.resolve(response.arguments.torrents);
-          });
-        });
-        return defer.promise;
+      torrents: function(transmissionRPC, GET_TORRENTS) {
+        return transmissionRPC.torrents(GET_TORRENTS);
       }
     }
   });
